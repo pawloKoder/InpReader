280aa3aa2e2e269c50c2d4fe053bcc6cf427c121
Paweł Wawruch <pawlo@aleg.pl>
2015-09-01 23:04:22 -0700

    Add missing settings file.

---
+++ src/inpreader/settings.h
@@ -0,0 +1,13 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+
+namespace InpReader {
+
+const bool verboseUnknownKeyword = false;
+const bool verboseAppendToModel = true;
+const bool verboseAppendToStep = true;
+
+}
+
+#endif //SETTINGS_H
