e6fe9d7bdebed5a38d77b12c0f4a3b1cbe69caf2
Paweł Wawruch <pawlo@aleg.pl>
2015-11-04 00:08:32 +0100

    Support NodeSet generation

--- src/inpreader/keywords/nodeset.cpp
+++ src/inpreader/keywords/nodeset.cpp
@@ -8,15 +8,26 @@
 const std::string Keywords::NodeSet::keyName = "Nset";
 
 
-Keywords::NodeSet::NodeSet(Keywords::Keyword* parent): Keyword(parent)
+Keywords::NodeSet::NodeSet(Keywords::Keyword* parent): Keyword(parent),
+	nodeGeneration(false)
 {}
 
 
 Keywords::Keyword* Keywords::NodeSet::appendDataLine(const Tokens::DataLine * line)
 {
-	for (size_t i = 0; i < line->data.size(); ++i)
-		if (!line->data[i].empty())
-			nset.nodes.push_back(std::stod(line->data[i]));
+	if (!nodeGeneration) {
+		for (size_t i = 0; i < line->data.size(); ++i)
+			if (!line->data[i].empty())
+				nset.nodes.push_back(std::stoi(line->data[i]));
+	} else {
+		if (line->data.size() >= 3) {
+			int n1 = std::stod(line->data[0]);
+			int n2 = std::stod(line->data[1]);
+			int increment = std::stod(line->data[2]);
+			for (int i = n1; i <= n2; i += increment)
+				nset.nodes.push_back(i);
+		}
+	}
 
 	return this;
 }
@@ -35,4 +46,8 @@ void Keywords::NodeSet::addParams(const std::map< std::string, std::string >& pa
 	auto t = params.find("nset");
 	if (t != params.end())
 		nset.name = t->second;
+	
+	auto g = params.find("generate");
+	if (g != params.end())
+		nodeGeneration = true;
 }
--- src/inpreader/keywords/nodeset.h
+++ src/inpreader/keywords/nodeset.h
@@ -25,6 +25,7 @@ public:
 
 private:
 	InpReader::NodeSet nset;
+	bool nodeGeneration;
 };
 
 }
